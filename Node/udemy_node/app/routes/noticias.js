25f174ea587cf9fc19c02af8fbca7c4ab0edb0c3
cin_rcorradi <cin_rcorradi@uolinc.com>
2018-08-02 18:52:33 -0300

    [master]  : Refatorando a conexão com o banco através do consign

--- Node/udemy_node/app/routes/noticias.js
+++ Node/udemy_node/app/routes/noticias.js
@@ -1,9 +1,6 @@
-var dbConection = require('../../config/dbConection');
-
 module.exports = function (server) {
-    var conection = dbConection();
-
     server.get('/noticias', function (req, res) {
+        var conection = server.config.dbConnection();
         conection.query('select * from noticias', function (error, result) {
             res.render('noticias/noticias.ejs', {noticias: result});
         });
--- Node/udemy_node/config/dbConnection.js
+++ Node/udemy_node/config/dbConnection.js
@@ -1,10 +1,13 @@
 var mySql = require('mysql');
-
-module.exports = function () {   
+var connectMysql = function () {
     return mySql.createConnection({
         host: 'localhost',
         user: 'root',
         password: '1234',
         database: 'portal_noticias'
     });
+}
+
+module.exports = function () {
+    return connectMysql;
 }
